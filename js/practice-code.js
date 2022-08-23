228421738d59f622aa95c6c8bcb58fc8cb2b646a
Mason Wudtee <mason.wudtee@gmail.com>
2022-08-23 14:59:30 -0500

    first 4 freeCodeCamp exercises commit

---
+++ js/practice-code.js
@@ -0,0 +1,20 @@
+//FreeCodeCamp.org
+//Let's get green squares!
+
+// (1/113) Commenting code
+/* Multi-line comment */
+
+// (2/113) Declaring variables.
+// var myName;
+
+// (3/113) Assigning a variable:
+//var a;
+// a = 7;
+
+// (4/113) Assigning the value of one variable to another
+// var a;
+// a = 7;
+// var b;
+// b = a;
+
+
