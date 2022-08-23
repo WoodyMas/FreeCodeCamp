9936173a1bc48c94415c495ae763814bf2288cf7
Mason Wudtee <mason.wudtee@gmail.com>
2022-08-23 15:24:51 -0500

    understanding iteration through an array with a for loop

--- js/practice-code.js
+++ js/practice-code.js
@@ -1,20 +1,58 @@
 //FreeCodeCamp.org
 //Let's get green squares!
 
-// (1/113) Commenting code
-/* Multi-line comment */
+// Learning comments
+    // Commenting code
+    /* Multi-line comment */
 
-// (2/113) Declaring variables.
-// var myName;
+// Declaring variables.
+    // var myName;
 
-// (3/113) Assigning a variable:
-//var a;
-// a = 7;
+// Assigning a variable:
+    //var a;
+    // a = 7;
 
-// (4/113) Assigning the value of one variable to another
-// var a;
-// a = 7;
-// var b;
-// b = a;
+// Assigning the value of one variable to another
+    // var a;
+    // a = 7;
+    // var b;
+    // b = a;
 
+// Initialize variables with the assignment operator
+    // var a = 9;
 
+// Declare String Variables
+    //let myFirstName = "Mason";
+    // let myLastName = "Wudtee";
+
+// Understanding Uninitialized Variables
+// Only change code below this line
+// Only change code below this line
+    // var a;
+    // var b;
+    // var c;
+    //
+    // a = 5;
+    // b = 10;
+    // c = "I am a"
+    //
+    //
+    // // Only change code above this line
+    //
+    // a = a + 1;
+    // b = b + 5;
+    // c = c + " String!";`
+
+// Iterate Through an Array with a For Loop
+//     // Setup
+//     const myArr = [2, 3, 4, 5, 6];
+//
+//     // Only change code below this line
+//     let total = 0;
+//     for (var i = 0; i < myArr.length; i++){
+//         console.log(myArr[i]);
+//         total += myArr[i];
+//
+//     }
+//
+//     console.log(total);
