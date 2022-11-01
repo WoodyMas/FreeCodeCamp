83b057ab64810e803247b590f7fb16f207d85f86
Mason Wudtee <mason.wudtee@gmail.com>
2022-11-01 18:11:57 -0500

    almost complete with FreeCodeCamp JS exercises

--- js/practice-code.js
+++ js/practice-code.js
@@ -58,3 +58,51 @@
 //     console.log(total);
 
 // Profile lookup (objects)
+function lookUpProfile(name, prop) {
+    // Only change code below this line
+    const result = contacts.filter(contact => contact.firstName === name);
+
+    if (result.length > 0) {
+        if (result[0][prop]) {
+            return result[0][prop];
+        } else {
+            return "No such property";
+        }
+    } else {
+        return "No such contact";
+    }
+}
+
+// Generate Random Fractions with JavaScript
+function randomFraction() {
+
+    // Only change code below this line
+
+    return Math.random(0 *1);
+
+    // Only change code above this line
+}
+
+// Generate Random Whole Numbers with JavaScript
+function randomWholeNum() {
+    // Only change code below this line
+    return Math.floor(Math.random() * 10);
+}
+
+// Generate Random Whole Numbers within a Range
+function randomRange(myMin, myMax) {
+    // Only change code below this line
+    return Math.floor(Math.random() * (myMax - myMin +1) + myMin);
+    // Only change code above this line
+}
+
+// Use the parseInt Function
+function convertToInteger(str) {
+    return parseInt(str);
+}
+
+// Use the parseInt Function with a Radix
+function convertToInteger(str) {
+    return parseInt(str, 2);
+}
+
