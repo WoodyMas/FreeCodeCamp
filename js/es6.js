66fba2d086cc32f0078663483e86f723f329c1b8
Mason Wudtee <mason.wudtee@gmail.com>
2022-11-02 12:16:31 -0500

    Problem 8 of 29. Use the Spread Operator to Evaluate Arrays In-Place

--- js/es6.js
+++ js/es6.js
@@ -74,3 +74,18 @@ const increment = (number, value = 1) => number + value;
 const sum = (...args) => {
     return args.reduce((a, b) => a + b, 0);
 }
+
+// Use the Spread Operator to Evaluate Arrays In-Place
+//// Original code
+    // const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+    // let arr2;
+
+    // arr2 = [];  // Change this line
+
+    // console.log(arr2);
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+
+arr2 = [...arr1];  // Change this line
+
+console.log(arr2);
