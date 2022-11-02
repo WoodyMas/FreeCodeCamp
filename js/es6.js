9c9767f9de1c91ad589b2ef6f325e79897d55211
Mason Wudtee <mason.wudtee@gmail.com>
2022-11-02 11:53:37 -0500

    Problem 5 of 29. Write Arrow Functions with Parameters

--- js/es6.js
+++ js/es6.js
@@ -47,3 +47,13 @@ function freezeObj() {
 //   return new Date();
 // };
 const magic = () => new Date();
+
+// Write Arrow Functions with Parameters
+//// Original code
+// var myConcat = function(arr1, arr2) {
+//   return arr1.concat(arr2);
+// };
+
+// console.log(myConcat([1, 2], [3, 4, 5]));
+const myConcat = ((arr1, arr2) => arr1.concat(arr2));
+console.log(myConcat("Good", " Fellas"));;
