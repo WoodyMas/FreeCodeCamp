f0ab6eace32b635e6540beed5f4df24562a7a090
Mason Wudtee <mason.wudtee@gmail.com>
2022-11-04 14:45:58 -0500

    Problem 15 of 29 solved. Create Strings using Template Literals

--- js/es6.js
+++ js/es6.js
@@ -254,7 +254,24 @@
     // const failuresList = makeList(result.failure);
 
 //// Solution
-
+    //const result = {
+    //   success: ["max-length", "no-amd", "prefer-arrow-functions"],
+    //   failure: ["no-var", "var-on-top", "linebreak"],
+    //   skipped: ["no-extra-semi", "no-dup-keys"]
+    // };
+    // function makeList(arr) {
+    //   // Only change code below this line
+    //   const failureItems = [];
+    //   arr.forEach(prop =>{
+    //     failureItems.push(`<li class="text-warning">${prop}</li>`);
+    //   });
+    //   // Only change code above this line
+    //
+    //   return failureItems;
+    // }
+    //
+    // const failuresList = makeList(result.failure);
+    // console.log(failuresList);
 
 
 // =================================================== Problem 16 ======================================================
