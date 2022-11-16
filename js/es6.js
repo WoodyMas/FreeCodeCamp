4080b51bc2e323c730cdec552f29d920a5f7b3d9
Mason Wudtee <mason.wudtee@gmail.com>
2022-11-16 15:10:35 -0600

    Problem 29 of 29 solved. Handle a Rejected Promise with catch

--- js/es6.js
+++ js/es6.js
@@ -568,6 +568,37 @@
 
 // =================================================== Problem 29 ======================================================
 
+// Handle a Rejected Promise with catch
 //// Original code
+    // const makeServerRequest = new Promise((resolve, reject) => {
+    //   // responseFromServer is set to false to represent an unsuccessful response from a server
+    //   let responseFromServer = false;
+    //
+    //   if(responseFromServer) {
+    //     resolve("We got the data");
+    //   } else {
+    //     reject("Data not received");
+    //   }
+    // });
+    //
+    // makeServerRequest.then(result => {
+    //   console.log(result);
+    // });
 
 //// Solution
+    //const makeServerRequest = new Promise((resolve, reject) => {
+    //   // responseFromServer is set to false to represent an unsuccessful response from a server
+    //   let responseFromServer = false;
+    //
+    //   if(responseFromServer) {
+    //     resolve("We got the data");
+    //   } else {
+    //     reject("Data not received");
+    //   }
+    // });
+    //
+    // makeServerRequest.then(result => {
+    //   console.log(result);
+    // }).catch(error => {
+    //   console.log(error);
+    // });
