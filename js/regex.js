f5849dd973584d693add88ea8c3e51f2815d0cf5
Mason Wudtee <mason.wudtee@gmail.com>
2022-11-19 15:33:56 -0600

    // Restrict Possible Usernames

--- js/regex.js
+++ js/regex.js
@@ -263,7 +263,17 @@
     // let result = movieName.match(noNumRegex).length;
 
 ///////////////////////////////////////////////////// Problem 22 ///////////////////////////////////////////////////////
-// Restrict
+// Restrict Possible Usernames
+// Change the regex userCheck to fit the constraints listed above.
+
+    //let username = "JackOfAllTrades";
+    // let userCheck = /change/; // Change this line
+    // let result = userCheck.test(username);
+
+// Solution
+    // let username = "JackOfAllTrades";
+    // const userCheck = /^[a-z]([0-9]{2,}|[a-z]+\d*)$/i;
+    // let result = userCheck.test(username);
 
 ///////////////////////////////////////////////////// Problem 23 ///////////////////////////////////////////////////////
 ///////////////////////////////////////////////////// Problem 24 ///////////////////////////////////////////////////////
