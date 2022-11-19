d1360a5eef138c999ee3cd7dbed91587abb7b80d
Mason Wudtee <mason.wudtee@gmail.com>
2022-11-19 12:24:40 -0600

    // Problem 10 of 33: Match Numbers and Letters of the Alphabet

--- js/regex.js
+++ js/regex.js
@@ -104,6 +104,18 @@
     // let result = quoteSample.match(alphabetRegex); // Changed
 
 ///////////////////////////////////////////////////// Problem 10 ///////////////////////////////////////////////////////
+// Problem 10 of 33: Match Numbers and Letters of the Alphabet
+    // let quoteSample = "Blueberry 3.141592653s are delicious.";
+    // let myRegex = /change/; // Change this line
+    // let result = myRegex; // Change this line
+
+    //Create a single regex that matches a range of letters between h and s, and a range of numbers between 2 and 6. Remember to include the appropriate flags in the regex.
+
+// Solution
+    // let quoteSample = "Blueberry 3.141592653s are delicious.";
+    // let myRegex = /[h-s2-6]/gi; // Changed
+    // let result = quoteSample.match(myRegex); // Changed
+
 ///////////////////////////////////////////////////// Problem 11 ///////////////////////////////////////////////////////
 ///////////////////////////////////////////////////// Problem 12 ///////////////////////////////////////////////////////
 ///////////////////////////////////////////////////// Problem 13 ///////////////////////////////////////////////////////
