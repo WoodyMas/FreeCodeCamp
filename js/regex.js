fa3e692c313dc2015ac7fbbe38191476871852fd
Mason Wudtee <mason.wudtee@gmail.com>
2022-11-23 15:26:12 -0600

    Use Capture Groups to Search and Replace

--- js/regex.js
+++ js/regex.js
@@ -390,7 +390,10 @@
     // let result = str.replace(fixRegex, replaceText);
 
 // Solution
-
+// let str = "one two three";
+// let fixRegex = /(\w+)\s(\w+)\s(\w+)/; // Change this line
+// let replaceText = "$3 $2 $1"; // Change this line
+// let result = str.replace(fixRegex, replaceText);
 
 ///////////////////////////////////////////////////// Problem 33 ///////////////////////////////////////////////////////
 
