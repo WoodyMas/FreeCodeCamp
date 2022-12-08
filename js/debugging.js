cb041f3f89a7b5805895467f17aa4f17cf6d0b77
Mason Wudtee <mason.wudtee@gmail.com>
2022-12-07 20:22:05 -0600

    debugging.js added and first solution found

---
+++ js/debugging.js
@@ -0,0 +1,21 @@
+// Use the JavaScript Console to Check the Value of a Variable
+
+//let a = 5;
+// let b = 1;
+// a++;
+// // Only change code below this line
+//
+//
+// let sumAB = a + b;
+// console.log(sumAB);
+
+// Use the console.log() method to print the value of the variable a where noted in the code.
+
+    let a = 5;
+    let b = 1;
+    a++;
+    // Only change code below this line
+    console.log(a);
+
+    let sumAB = a + b;
+    console.log(sumAB);
