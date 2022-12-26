5a09e7fd970ab0462c5635ffeadb793ec2ea948c
Mason Wudtee <mason.wudtee@gmail.com>
2022-12-26 12:36:41 -0600

    Catch Misspelled Variable and Function Names

--- js/debugging.js
+++ js/debugging.js
@@ -1,13 +1,13 @@
 // Use the JavaScript Console to Check the Value of a Variable
 
-//let a = 5;
-// let b = 1;
-// a++;
-// // Only change code below this line
-//
-//
-// let sumAB = a + b;
-// console.log(sumAB);
+    //let a = 5;
+    // let b = 1;
+    // a++;
+    // // Only change code below this line
+    //
+    //
+    // let sumAB = a + b;
+    // console.log(sumAB);
 
 // Use the console.log() method to print the value of the variable a where noted in the code.
 
@@ -22,23 +22,31 @@
 
 // Understanding the Differences between the freeCodeCamp and Browser Console
 
-// First, open your browser console so you can see the logs. To do that, you can right-click the freeCodeCamp navigation bar at the top and click inspect on most browsers. Then find the console tab in the window that opens.
-//
-// After that, use console.log to log the output variable. View the two consoles to see the log. Finally, use console.clear after your log to clear the browser console. View the difference in the two consoles.
+    // First, open your browser console so you can see the logs. To do that, you can right-click the freeCodeCamp navigation bar at the top and click inspect on most browsers. Then find the console tab in the window that opens.
+    //
+    // After that, use console.log to log the output variable. View the two consoles to see the log. Finally, use console.clear after your log to clear the browser console. View the difference in the two consoles.
 
 // let output = "Get this to show once in the freeCodeCamp console and not at all in the browser console";
 //
 
-// Solution
-// console.log(output);
-// console.clear();
+    // Solution
+    // console.log(output);
+    // console.clear();
 
 // Use typeof to Check the Type of a Variable
 
-// let seven = 7;
-// let three = "3";
-// console.log(seven + three);
-// // Only change code below this line
-// console.log(typeof seven);
-//
-// console.log(typeof three);
+    // let seven = 7;
+    // let three = "3";
+    // console.log(seven + three);
+    // // Only change code below this line
+    // console.log(typeof seven);
+    //
+    // console.log(typeof three);
+
+// Catch Misspelled Variable and Function Names
+
+    // let receivables = 10;
+    // let payables = 8;
+    // let netWorkingCapital = receivables - payables;
+    // console.log(`Net working capital is: ${netWorkingCapital}`);
+
