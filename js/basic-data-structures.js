b73b17a4e77d6a3ebc3975ad2f986002ce5d3276
Mason Wudtee <mason.wudtee@gmail.com>
2022-12-26 13:58:08 -0600

    Access an Array's Contents Using Bracket Notation

--- js/basic-data-structures.js
+++ js/basic-data-structures.js
@@ -9,3 +9,14 @@
     //     "one number",
     //     "one boolean"
     // ]; // Change this line
+
+
+// Access an Array's Contents Using Bracket Notation
+
+// In order to complete this challenge, set the 2nd position (index 1) of myArray to anything you want, besides the letter b.
+
+    // let myArray = ["a", "b", "c", "d"];
+    // // Only change code below this line
+    // myArray[1] = "anything you want, besides the letter b";
+    // // Only change code above this line
+    // console.log(myArray);
