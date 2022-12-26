fb23e38ae855c98ce026e9b1c342d828a00faeac
Mason Wudtee <mason.wudtee@gmail.com>
2022-12-26 12:50:13 -0600

    Catch Missing Open and Closing Parenthesis After a Function Call

--- js/debugging.js
+++ js/debugging.js
@@ -74,3 +74,14 @@
     // }
     //
     // console.log(result);
+
+// Catch Missing Open and Closing Parenthesis After a Function Call
+
+    // function getNine() {
+    //     let x = 6;
+    //     let y = 3;
+    //     return x + y;
+    // }
+    //
+    // let result = getNine();
+    // console.log(result);
