6a0bde70bdd4ae039c427163f894e51f4e815f42
Mason Wudtee <mason.wudtee@gmail.com>
2022-12-26 13:41:17 -0600

    Use Caution When Reinitializing Variables Inside a Loop

--- js/debugging.js
+++ js/debugging.js
@@ -100,3 +100,46 @@
     // console.log(power);
     //
     // console.log(Math.pow(2, 3));
+
+// Catch Off By One Errors When Using Indexing
+
+// Fix the two indexing errors in the following function so all the numbers 1 through 5 are printed to the console.
+    // function countToFive() {
+    //     let firstFive = "12345";
+    //     let len = firstFive.length;
+    //     // Only change code below this line
+    //     for (let i = 0; i < len; i++) {
+    //         // Only change code above this line
+    //         console.log(firstFive[i]);
+    //     }
+    // }
+    //
+    // countToFive();
+
+
+// Use Caution When Reinitializing Variables Inside a Loop
+
+// TODO: The following function is supposed to create a two-dimensional array with m rows and n columns of zeroes. Unfortunately, it's not producing the expected output because the row variable isn't being reinitialized (set back to an empty array) in the outer loop. Fix the code so it returns a correct 3x2 array of zeroes, which looks like [[0, 0], [0, 0], [0, 0]].
+function zeroArray(m, n) {
+    // Creates a 2-D array with m rows and n columns of zeroes
+    let newArray = [];
+    let row = [];
+    for (let i = 0; i < m; i++) {
+        // Adds the m-th row into newArray
+
+        for (let j = 0; j < n; j++) {
+            // Pushes n zeroes into the current row to create the columns
+            row.push(0);
+        }
+        // Pushes the current row, which now has n zeroes in it, to the array
+        newArray.push(row);
+
+        // INSERTED: row needed to be reset to an empty array
+        row = [];
+    }
+    return newArray;
+}
+
+let matrix = zeroArray(3, 2);
+console.log(matrix);
+
