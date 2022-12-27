f930522152c886a8f2cfb6f18150f385c6d0de87
Mason Wudtee <mason.wudtee@gmail.com>
2022-12-26 19:47:25 -0600

    Generate an Array of All Object Keys with Object.keys()

--- js/basic-data-structures.js
+++ js/basic-data-structures.js
@@ -367,3 +367,40 @@
     // }
     //
     // console.log(countOnline(users));
+
+
+// Generate an Array of All Object Keys with Object.keys()
+
+// Finish writing the getArrayOfUsers function so that it returns an array containing all the properties in the object it receives as an argument.
+
+    // let users = {
+    //   Alan: {
+    //     age: 27,
+    //     online: false
+    //   },
+    //   Jeff: {
+    //     age: 32,
+    //     online: true
+    //   },
+    //   Sarah: {
+    //     age: 48,
+    //     online: false
+    //   },
+    //   Ryan: {
+    //     age: 19,
+    //     online: true
+    //   }
+    // };
+    //
+    // function getArrayOfUsers(obj) {
+    //   // Only change code below this line
+    //   let objArray = [];
+    //   for (let user in obj) {
+    //     objArray.push(user);
+    //
+    //   }
+    //   return objArray
+    //   // Only change code above this line
+    // }
+    //
+    // console.log(getArrayOfUsers(users));
