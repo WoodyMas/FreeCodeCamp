642cd8a1ced0f918f727cc3fac3a6b7382f099f6
Mason Wudtee <mason.wudtee@gmail.com>
2022-12-28 14:46:52 -0600

    factorialize a number

--- js/basic-algorithm-scripting.js
+++ js/basic-algorithm-scripting.js
@@ -42,6 +42,33 @@
 
 // =================================================== Problem 3 =======================================================
 
+// Factorialize a Number
+
+// Return the factorial of the provided integer.
+//
+// If the integer is represented with the letter n, a factorial is the product of all positive integers less than or equal to n.
+//
+// Factorials are often represented with the shorthand notation n!
+//
+// For example: 5! = 1 * 2 * 3 * 4 * 5 = 120
+//
+// Only integers greater than or equal to zero will be supplied to the function.
+
+// Solution
+
+    // function factorialize(num) {
+    //   if (num < 0) {
+    //     return -1;
+    //   } else if (num == 0 || num == 1) {
+    //     return 1;
+    //   } else {
+    //     return (num * factorialize(num -1));
+    //   }
+    // }
+    //
+    // factorialize(5);
+
+
 // =================================================== Problem 4 =======================================================
 
 // =================================================== Problem 5 =======================================================
