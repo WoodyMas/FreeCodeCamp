000d3d85e50e666b3ec8fa39014751e7a8dfa6c4
Mason Wudtee <mason.wudtee@gmail.com>
2022-12-29 21:45:41 -0600

    Slice and Splice

--- js/basic-algorithm-scripting.js
+++ js/basic-algorithm-scripting.js
@@ -292,6 +292,30 @@
 
 // =================================================== Problem 12 =======================================================
 
+// Slice and Splice
+
+// You are given two arrays and an index.
+//
+// Copy each element of the first array into the second array, in order.
+//
+// Begin inserting elements at index n of the second array.
+//
+// Return the resulting array. The input arrays should remain the same after the function runs.
+
+//// My Solution
+
+    //   function frankenSplice(arr1, arr2, n) {
+    //     let newArr =[];
+    //     newArr.push(...arr2);
+    //     newArr.splice(n, 0, ...arr1);
+    //     return newArr;
+    //   }
+    //
+    //   console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
+    //   console.log(frankenSplice(["claw", "tentacle"], ["head", "shoulders", "knees", "toes"], 2));
+    //   console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
+
+
 // =================================================== Problem 13 =======================================================
 
 // =================================================== Problem 14 =======================================================
